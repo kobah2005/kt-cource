ef70c9d712b7819f85c39bc41064a35338dd9aa5
ymitrofanov <ymitrofanov@openintegration.inc>
2025-02-09 12:06:38 +0300

    add kotlin course base dir

--- src/main/kotlin/Collections101.kt
+++ src/main/kotlin/Collections101.kt
@@ -1,3 +1,27 @@
-fun main () {
+//fun main () {
+ /* задача 1
+    val family = listOf<String>("one","two","three")
+    val one= family[0]
+    println(one)
 
+  */
+
+/* задача 2
+    val nDay = listOf<Int>(31,28,31,30,31,30,31,31,30,31,30,31)
+    val nMonth = readln().toInt()
+    if( nMonth<1 || nMonth>12){println("такоого месяца нет")
+    } else {
+    println("в $nMonth месяце ${nDay[nMonth-1]} дней")
+    }
+
+ */
+/* задача 3
+fun main () {
+    val mons = listOf<String>("Январь","Февраль","Март","Апрель","Май","Июнь","Июль","Август","Сентябрь","Октябрь","Ноябрь","Декабрь")
+    val nMonth = readln().toInt()
+    if( nMonth<1 || nMonth>12){println("\"Некорректное значение: $nMonth")
+    } else {
+        println(mons[nMonth-1])
+    }
 }
+*/
---
+++ src/main/kotlin/HapyB.kt
@@ -0,0 +1,8 @@
+fun main() {
+    val age = readln().toInt()
+    repeat(age) {
+        println("Happy birthday")
+    }
+
+
+}
